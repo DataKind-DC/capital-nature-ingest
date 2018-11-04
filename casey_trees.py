29bcccd38b720908640b20785f3e7c8503063e0b
Aimee Barciauskas <aimee@developmentseed.org>
2018-11-04 10:08:22 -0500

    More events from casey trees and activity Categories

--- casey_trees.py
+++ casey_trees.py
@@ -13,61 +13,69 @@ elasticsearch_domain = os.environ['ELASTICSEARCH_DOMAIN']
 pp = pprint.PrettyPrinter(indent=2)
 
 # create request to get data from Casey Trees calendar
-r = requests.get('https://caseytrees.org/events/month/')
-base_url = get_base_url(r.text, r.url)
-data = extruct.extract(r.text, base_url = base_url, syntaxes=['json-ld'], uniform=True)
+months = map(lambda m: "2018-{0}".format(m), range(10,13))
+activityCategories = ['Tree Planting', 'Park Inventory', 'Tree Identification']
 
-# get json-ld data for Casey Trees
-event_json = []
-for jsonLD in data['json-ld']:
-    if jsonLD['@type'] == 'Event':
-        event_json.append(jsonLD)
+for month in months:
+  r = requests.get("https://caseytrees.org/events/{0}/".format(month))
+  base_url = get_base_url(r.text, r.url)
+  data = extruct.extract(r.text, base_url = base_url, syntaxes=['json-ld'], uniform=True)
+
+  # get json-ld data for Casey Trees
+  event_json = []
+  for jsonLD in data['json-ld']:
+      if jsonLD['@type'] == 'Event':
+          event_json.append(jsonLD)
 
 
-for individual_event in event_json:
-  # establish list of properties in dict
-  key_list = [
-    'name',
-    'startDate',
-    'endDate',
-    'geo',
-    'url',
-    'image',
-    'description',
-    'registrationRequired',
-    'registrationByDate',
-    'registrationURL',
-    'fee',
-    'location',
-    'organization',
-    'offers',
-    'physicalRequirements',
-    'activityCategory'
-  ]
-  
-  event_data = {k:None for k in key_list}
+  for individual_event in event_json:
+    # establish list of properties in dict
+    key_list = [
+      'name',
+      'startDate',
+      'endDate',
+      'geo',
+      'url',
+      'image',
+      'description',
+      'registrationRequired',
+      'registrationByDate',
+      'registrationURL',
+      'fee',
+      'location',
+      'organization',
+      'offers',
+      'physicalRequirements',
+      'activityCategory'
+    ]
+    
+    event_data = {k:None for k in key_list}
 
-  for k in individual_event:
-    # if property from keylist exists within the event_json object, set property
-    if k in key_list:
-      event_data[k] = individual_event[k]
+    for k in individual_event:
+      # if property from keylist exists within the event_json object, set property
+      if k in key_list:
+        event_data[k] = individual_event[k]
 
-    event_data['geo'] = {}
-    # Prevent key errors
-    if 'location' in individual_event and 'geo' in individual_event['location']:
-      event_data['geo']['lat'] = individual_event['location']['geo']['latitude']
-      event_data['geo']['lon'] = individual_event['location']['geo']['longitude']
-  event_data['ingested_by'] = 'https://github.com/DataKind-DC/capital-nature-ingest/blob/master/casey_trees.py'
+      event_data['geo'] = {}
+      # Prevent key errors
+      if 'location' in individual_event and 'geo' in individual_event['location']:
+        event_data['geo']['lat'] = individual_event['location']['geo']['latitude']
+        event_data['geo']['lon'] = individual_event['location']['geo']['longitude']
+    
+    for cat in activityCategories:
+      if cat in individual_event['name']: event_data['activityCategory'] = cat 
 
-  # check each url to determine if event is duplicate
-  event_id = filter(None, event_data['url'].split('/'))[-1]
+    event_data['ingested_by'] = 'https://github.com/DataKind-DC/capital-nature-ingest/blob/master/casey_trees.py'
 
-  # create the request to submit data to elasticsearch
-  r = requests.put(
-    "{0}/capital_nature/event/{1}".format(elasticsearch_domain, event_id),
-    data=json.dumps(event_data),
-    headers = {'content-type': 'application/json'})
+    # check each url to determine if event is duplicate
+    event_id = filter(None, event_data['url'].split('/'))[-1]
 
-  # print status code
-  # TODO: handle response, provide error messaging if necessary
-  pp.pprint(r.status_code)
+    # create the request to submit data to elasticsearch
+    r = requests.put(
+      "{0}/capital_nature/event/{1}".format(elasticsearch_domain, event_id),
+      data=json.dumps(event_data),
+      headers = {'content-type': 'application/json'})
+
+    # print status code
+    # TODO: handle response, provide error messaging if necessary
+    pp.pprint(r.status_code)
