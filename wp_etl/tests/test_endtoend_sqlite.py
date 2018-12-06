88b559b37a87b31da0b7e2bcf95a3d0a5ab6a051
Dominic White <dewhite4@gmail.com>
2018-12-06 00:16:25 -0500

    TEST: End to end test (SQLite)

---
+++ wp_etl/tests/test_endtoend_sqlite.py
@@ -0,0 +1,15 @@
+import unittest
+import sqlite3
+
+from wp_etl.csv_parser import CSVParser
+from wp_etl.tests.testing_utilities import SqliteDatabaseLoader
+
+class EndToEndLocalTest(unittest.TestCase):
+
+    def test_end_to_end_runs(self):
+        cp = CSVParser()
+        cp.open("result.csv")
+        cp.parse()
+
+        dbl = SqliteDatabaseLoader()
+        dbl.load_events(cp.parsed_events)
