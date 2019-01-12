89b8849baa8e836a952c9be562f8648efcff2100
csmcallister <mcallistercs@gmail.com>
2019-01-11 21:52:58 -0500

    ENH: match csv headers to wordpress schema and id recurring events

--- lambdas/nps/lambda_function.py
+++ lambdas/nps/lambda_function.py
@@ -1,7 +1,6 @@
 import requests
 import os
 import re
-import json
 import sys
 import csv
 from bs4 import BeautifulSoup
@@ -20,40 +19,18 @@ except KeyError:
     sys.exit(1)
 
 
-def get_park_codes_by_state(state, limit = 200):
-    '''
-    Given a state (e.g. 'DC'), return all of its NPS park codes. These are used as params for the NPS events API
-    Parameters:
-        state (str): a two character string for a state abbreviation (eg. 'DC')
-        limit (int): number of results to return per request. Default is 200.
-    Returns:
-        park_codes (list): a list of str 4-char park codes to be used with the NPS Events API
-    '''
-
-    key_param = f'&api_key={NPS_KEY}'
-    limit_param = f'&limit={limit}'
-    url = "https://developer.nps.gov/api/v1/parks?stateCode=" + state + key_param + limit_param
-    r = requests.get(url)
-    data = r.json()
-    park_codes = []
-    for park in data["data"]:
-        park_codes.append(park["parkCode"])
-
-    return park_codes
-
-def get_park_events(park_code, limit=200):
+def get_park_events(park_code, limit=1000):
     '''
     Get events data from the National Parks Service Events API for a given park_code
         
     Parameters:
         park_code (str): a park_code as returned by the NPS parkCode API through get_park_codes_by_state()
-        limit (int): number of results to return per request. Default is 200
+        limit (int): number of results to return per request. Default is 1000
     
     Returns:
         park_events (list): A list of dicts representing each event with 'park' as the siteType. 
-                            The dict structures follows that of the NPS Events API.
+                            The dict structures follow that of the NPS Events API.
     '''
-
     park_code_param = f'?parkCode={park_code}'
     limit_param = f'&limit={limit}'
     key_param = f'&api_key={NPS_KEY}'
@@ -68,15 +45,23 @@ def get_park_events(park_code, limit=200):
     
     return park_events
 
-def get_nps_events(park_codes):
+
+def get_nps_events():
     '''
-    Get all of the events associated with a park code
+    Get all of the events for each park in the park_codes list 
     Parameters:
-        park_codes (list): a list of str 4-char park codes.
+        None
     Returns:
-        nps_events (list): a list of events
+        nps_events (list): a list of events, as returned by get_park_events()
     '''
-    
+    park_codes = ['afam','anac','anti','apco','appa','arho','asis','balt','bawa','bepa','blri',
+                  'bowa','cahi','cajo','came','cato','cawo','cbgn','cbpo','cebe','choh','clba',
+                  'coga','colo','cuga','cwdw','fodu','fofo','fomc','fomr','foth','fowa','frde',
+                  'frdo','frsp','gewa','glec','gree','grfa','grsp','gwmp','hafe','haha','hamp',
+                  'hatu','jame','jthg','keaq','kowa','linc','lyba','mamc','mana','mawa','mlkm',
+                  'mono','nace','nama','ovvi','oxhi','paav','pete','pisc','pohe','prwi','rich',
+                  'rocr','shen','shvb','stsp','this','thje','thst','vive','wamo','waro','whho',
+              'wotr','wwii','york']
     nps_events = []
     for park_code in park_codes:
         try:
@@ -89,61 +74,22 @@ def get_nps_events(park_codes):
        
     return nps_events
 
-def get_park_geo_data(park_codes):
-    '''
-    Given a park_code, use the NPS Parks API to obtain geo and location data to match our application's schema
-    Parameters:
-        park_codes (list): a list of park codes as strings as returned by get_park_codes_by_state()
-    Returns:
-        park_codes_geo_map (dict): a dict containing both the geo and location objects to insert into an event's schema
-    '''
-    
-    park_codes_geo_map = {k:None for k in park_codes}
-    for park_code in park_codes:
-        key_param = f'&api_key={NPS_KEY}'
-        endpoint = "https://developer.nps.gov/api/v1/parks?parkCode=" + park_code + key_param + "&fields=addresses"
-        r = requests.get(endpoint)
-        data = r.json()['data']
-        lat_lon = data[0]["latLong"]
-        try:
-            lat, lon = tuple([".".join(re.findall(r'\d+', x)) for x in lat_lon.split(",")])
-        except ValueError:
-            lat, lon = ('','')
-        name = data[0]['name']
-        description = data[0]['description']
-
-        city = ''
-        state = ''
-        postal_code = ''
-        street = ''
-        for address in data[0]["addresses"]:
-            if address["type"] == "Physical":
-                city += address["city"]
-                state += address["stateCode"]
-                postal_code += str(address["postalCode"])
-                street += address['line1'] + ' ' + address['line2'] + ' ' +address['line3']
-
-        geo_obj = {'lat':lat,
-                   'lon':lon}
-        location_obj = {'streetAddress':street,
-                        'addressLocality':city,
-                        'addressRegion':None,
-                        'postalCode':postal_code,
-                        'addressCountry':'US',
-                        'name':name,
-                        'description':description,
-                        'telephone':None} 
-    
-        park_codes_geo_map[park_code] = {'geo':geo_obj, 'location':location_obj, 'state':state}
-    
-    return park_codes_geo_map
 
 def get_specific_event_location(event_id):
+    '''
+    Some parks are large, and the events have a more specific location than just the park name.
+    To get this location, use the event id to find the event page and then scrape that specific 
+    location from the page.
+    
+    Parameters:
+        event_id (str): unique identifier for this event
+    Returns:
+        specific_event_location (str): the specific location of the event
+    '''
     website = f"https://www.nps.gov/planyourvisit/event-details.htm?id={event_id}"
     r = requests.get(website)
     content = r.content
     soup = BeautifulSoup(content, "html.parser")
-
     # kill all script and style elements
     for script in soup(["script", "style"]):
         script.extract()    # rip it out
@@ -155,9 +101,7 @@ def get_specific_event_location(event_id):
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
     # drop blank lines
     text = '\n'.join(chunk for chunk in chunks if chunk)
-
     #extract the data from the raw text
-    event_data = {}
     split_text = text.split("\n")
     specific_event_location = ''
     for i,line in enumerate(split_text):
@@ -169,109 +113,96 @@ def get_specific_event_location(event_id):
     
     return specific_event_location
 
-def shcematize_nps_event(nps_event, park_codes_geo_map):
-    '''
-    Converts the event data from the NPS API into our application's schema.
-    Parameters:
-        nps_event (dict): a dict representing the json of a single NPS event.
-        park_codes_geo_map (dict): a dict containing both the geo and location objects to insert into an event's schema
-    Returns:
-        event_id (str): unique identifier for this event
-        schematized_nps_event (dict): the event data in our application's schema    
-    '''
-    
-    event_id = nps_event['id']
-    name = nps_event['title']
-    startDate = nps_event['dateStart']
-    endDate = nps_event['dateEnd']
-    park_code = nps_event['siteCode']
-    geo = park_codes_geo_map[park_code]['geo']
-    state = park_codes_geo_map[park_code]['state']
-    regResURL = nps_event['regResURL']
-    infoURL = nps_event['infoURL']
-    portalName = nps_event['portalName']
-    if len(regResURL) > 0:
-        url = regResURL
-    else:
-        r = requests.get(f"https://www.nps.gov/planyourvisit/event-details.htm?id={event_id}")
-        if r.status_code == 404:
-            if len(portalName) > 0:
-                url = portalName
-            else:
-                url = infoURL
-        else:
-            url = f"https://www.nps.gov/planyourvisit/event-details.htm?id={event_id}"
-    image = nps_event['images']
-    description = nps_event['description']
-    registrationRequired = True if nps_event['isRegResRequired'] else 0
-    registrationByDate = nps_event['regResInfo']
-    registrationURL = nps_event['regResURL']
-    fee = 'free' if nps_event['isFree'] else nps_event['feeInfo']
-    location = park_codes_geo_map[park_code]['location']
-    organization = {'name':nps_event['organizationName'],
-                    'description':None,
-                    'url':None,
-                    #'telephone':nps_event['contactTelephoneNumber'],
-                    'email':nps_event['contactEmailAddress']}
-    activityCategories = nps_event['tags']
-    eventTypes = nps_event['types']
-    ingested_by = 'https://github.com/DataKind-DC/capital-nature-ingest/tree/master/ingest_scripts/nps.py'
-    #this is the old schema below
-    '''
-    schematized_nps_event = { 'name': name,
-                              'startDate': startDate,
-                              'endDate': endDate,
-                              'geo': geo,
-                              'url': url,
-                              'image': image,
-                              'description': description,
-                              'registrationRequired': registrationRequired,
-                              'registrationByDate': registrationByDate,
-                              'registrationURL': registrationURL,
-                              'fee': fee,
-                              'location': location,
-                              'organizationDetails': organization,
-                              'offers':None,
-                              'physicalRequirements':None,
-                              'activityCategories':activityCategories,
-                              'eventTypes':eventTypes,
-                              'ingested_by':ingested_by}
-    '''
-    #here's the new schema
-    specific_event_location = get_specific_event_location(event_id)
-    venueName = re.sub(' +', ' ', location['name'] + ", " + specific_event_location)
-    venueAddress = re.sub(' +', 
-                          ' ', 
-                          f"{location['streetAddress']} {location['addressLocality']}, {state} {location['postalCode']}, USA")
-    times = nps_event['times']
-    #TODO handle multiple start and end times
-    schematized_nps_event = [
-                              {
-                                "website": url, 
-                                "startDate": startDate, 
-                                "endDate": endDate, 
-                                "startTime": None, 
-                                "latitude": geo['lat'], 
-                                "venueName": venueName, 
-                                "endTime": None, 
-                                "longitude": geo['lon'], 
-                                "venueAddress": venueAddress
-                              }
-                            ]
 
-    return event_id, schematized_nps_event
+def shcematize_nps_event(nps_event):
+    '''
+    Extract data from the nps event so that it conforms to the wordpress schema.
+    
+    Parameters:
+        nps_event (dict): an NPS event, as returned by the NPS API
+    Returns:
+        schematized_nps_events (list): a list of dicts, with each dict representing an event
+    
+    '''
+    date_end = nps_event['dateStart']
+    date_start = nps_event['dateEnd']
+    if date_start == date_end:
+        schematized_nps_events = []
+        dates = nps_event['dates']
+        for date in dates:
+            times = nps_event['times']
+            for time in times:
+                event_start_time = time['timeStart']
+                event_end_time = time['timeEnd']
+                event_name = nps_event['title']
+                try:
+                    event_description = BeautifulSoup(nps_event['description'], "html.parser").find("p").text
+                except AttributeError:
+                    event_description = ''
+                event_all_day = nps_event['isAllDay']
+                event_id = nps_event['id']
+                specific_event_location = get_specific_event_location(event_id)
+                venue_name = re.sub(' +', ' ', nps_event['parkFullName'] + ", " + specific_event_location)
+                event_organization = nps_event['organizationName']
+                event_cost = 'free' if nps_event['isFree'] else nps_event['feeInfo']
+                event_category = nps_event['category']
+                event_tags = ", ".join(nps_event['tags'])
+                regResURL = nps_event['regResURL']
+                infoURL = nps_event['infoURL']
+                portalName = nps_event['portalName']
+                if len(regResURL) > 0:
+                    event_website = regResURL
+                else:
+                    r = requests.get(f"https://www.nps.gov/planyourvisit/event-details.htm?id={event_id}")
+                    if r.status_code == 404:
+                        if len(portalName) > 0:
+                            event_website = portalName
+                        else:
+                            event_website = infoURL
+                    else:
+                        event_website = f"https://www.nps.gov/planyourvisit/event-details.htm?id={event_id}"
+                try:
+                    event_image = nps_event['images'][0]['url'] 
+                except IndexError:
+                    event_image = None
+                if event_image:
+                    if "nps.gov" not in event_image:
+                        event_image = f"https://www.nps.gov{event_image}"
+                schematized_nps_event = {
+                                            "Event Name":event_name,
+                                            "Event Description":event_description,
+                                            "Event Start Date":date,
+                                            "Event Start Time":event_start_time,
+                                            "Event End Date":date,
+                                            "Event End Time":event_end_time,
+                                            "All Day Event":event_all_day,
+                                            "Event Venue Name":venue_name,
+                                            "Event Organizer Name(s) or ID(s)":event_organization,
+                                            "Event Cost":event_cost,
+                                            "Event Category":event_category,
+                                            "Event Tags":event_tags,
+                                            "Event Website":event_website,
+                                            "Event Featured Image":event_image
+                                          }
+                                        
+                schematized_nps_events.append(schematized_nps_event)
+    else:
+        #TODO maybe log these occurrences, which I don't think really occur given the API's schema 
+        schematized_nps_events = []
+
+    return schematized_nps_events
+
 
 def main():
-    va_codes = get_park_codes_by_state('VA')
-    dc_codes = get_park_codes_by_state('DC')
-    md_codes = get_park_codes_by_state('MD')
-    park_codes = set(va_codes + dc_codes + md_codes)
-    nps_events = get_nps_events(park_codes)
-    park_codes_geo_map = get_park_geo_data(park_codes)
-    events = {}
+    '''
+    Returns NPS events for VA, DC and MD. Each is a dict conforming to the wordpress schema.
+    '''
+    nps_events = get_nps_events()
+    events = []
     for nps_event in nps_events:
-        event_id, schematized_nps_event = shcematize_nps_event(nps_event, park_codes_geo_map)
-        events[event_id] = schematized_nps_event
+        schematized_nps_events = shcematize_nps_event(nps_event)
+        for schematized_nps_event in schematized_nps_events:
+            events.append(schematized_nps_event)
     
     return events
 
@@ -284,25 +215,29 @@ def nps_handler(event, context):
     source_name = event['source_name']
     events = main()
     filename = '{0}-results.csv'.format(source_name)
-    fieldnames = list(events[list(events.keys())[0]][0].keys())
+    fieldnames = list(events[0].keys())
     if not is_local:
         with open('/tmp/{0}'.format(filename), mode = 'w') as f:
             writer = csv.DictWriter(f, fieldnames = fieldnames)
             writer.writeheader()
-            for event_id in events:
-                nps_event = events[event_id][0]
+            for nps_event in events:
                 writer.writerow(nps_event)
         s3 = boto3.resource('s3')
         s3.meta.client.upload_file('/tmp/{0}'.format(filename), 
                                     bucket, 
                                     'capital-nature/{0}'.format(filename)
-                                    )  
+                                    )
+    else:
+        with open(filename, mode = 'w') as f:
+            writer = csv.DictWriter(f, fieldnames = fieldnames)
+            writer.writeheader()
+            for nps_event in events:
+                writer.writerow(nps_event)
 
-
-# For local testing
+# For local testing (it'll write the csv as nps-results.csv into your working dir)
 # event = {
 #   'url': 'https://nps.gov',
 #   'source_name': 'nps'
 # }
 # is_local = True
-# nps_handler(handler(event, {}))
+# nps_handler(event,None)
