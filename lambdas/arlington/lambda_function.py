e2744a3a85cf8a83213b57b80c5cb01572ba19c0
csmcallister <mcallistercs@gmail.com>
2019-01-11 22:34:58 -0500

    ENH: create placeholders for Arlington Parks and Recreation events

---
+++ lambdas/arlington/lambda_function.py
@@ -0,0 +1,15 @@
+import bs4
+import requests
+import csv
+import boto3
+
+bucket = 'aimeeb-datasets-public'
+is_local = False
+
+
+def main():
+  # TODO: scrape https://environment.arlingtonva.us/events/
+  pass
+  
+
+
