90e5281a552cb36c34697db04a6a56a90e8229c3
Dominic White <dewhite4@gmail.com>
2019-01-15 18:16:27 -0500

    Parse basic data from Timely calendar page

--- lambdas/rcc/lambda_function.py
+++ lambdas/rcc/lambda_function.py
@@ -14,49 +14,52 @@ def fetch_page(options):
 
 # Given a beautiful soup object, return a list of events on that page
 def handle_ans_page(soup):
-  events = soup.find_all('div', {'class': 'event'})
-  event_output = []
-  for e in events:
-    latLong = e.find('p').find('span', {'class': 'evcal_desc'})['data-latlng'].split(',')
-    spanTime = e.find('p').find('span', {'class': 'evo_time'})
-    event_data = {
-      'website': e.find('a')['href'],
-      'startDate': e.find('time', {'itemprop': 'startDate'})['datetime'],
-      'startTime': spanTime.find('span', {'class': 'start'}).text,
-      'endDate': e.find('time', {'itemprop': 'endDate'})['datetime'],
-      'endTime': spanTime.find('span', {'class': 'end'}).text.replace('- ', ''),
-      'venueName': e.find('span', {'itemprop': 'name'}).text,
-      'venueAddress': e.find('item', {'itemprop': 'streetAddress'}).text,
-      'latitude': float(latLong[0]),
-      'longitude': float(latLong[1])
-    }
-    event_output.append(event_data)
-  return event_output
+    events = soup.find_all('a', {'class': 'timely-event'})
+    event_output = []
+    for e in events:
+        event_data = {
+            'Event Name': e.find('div', {'class': 'timely-title'}).find('span').text,
+            'Event Organizer Name(s) or ID(s)': 'Rock Creek Conservancy',
+            'Event Venue Name': e.find('span', {'class': 'timely-venue'}).text.strip()[2:],
+            'Event Website': e.attrs['href'],
+            'Event Start Date': e.attrs['data-date'],
+            'Event Start Time': e.find('div', {'class': 'timely-start-time'}).text.strip(),
+            # 'Event End Date': None,
+            # 'Event End Time': None,
+        }
+        event_output.append(event_data)
+    return event_output
 
 def handler(event, context):
-  url = event['url']
-  source_name = event['source_name']
-  page = fetch_page({'url': url})
-  soup = bs4.BeautifulSoup(page, 'html.parser')
-  event_output = handle_ans_page(soup)
-  filename = '{0}-results.csv'.format(source_name)
-  if not is_local:
-    with open('/tmp/{0}'.format(filename), mode = 'w') as f:
-      writer = csv.DictWriter(f, fieldnames = event_output[0].keys())
-      writer.writeheader()
-      [writer.writerow(event) for event in event_output]
-    s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(
-      '/tmp/{0}'.format(filename),
-      bucket,
-      'capital-nature/{0}'.format(filename)
-    )  
-  return json.dumps(event_output, indent=2)
+    url = event['url']
+    source_name = event['source_name']
+    page = fetch_page({'url': url})
+    soup = bs4.BeautifulSoup(page, 'html.parser')
+    event_output = handle_ans_page(soup)
+    filename = '{0}-results.csv'.format(source_name)
+    if not is_local:
+        with open('/tmp/{0}'.format(filename), mode = 'w') as f:
+            writer = csv.DictWriter(f, fieldnames = event_output[0].keys())
+            writer.writeheader()
+            [writer.writerow(event) for event in event_output]
+        s3 = boto3.resource('s3')
+        s3.meta.client.upload_file(
+            '/tmp/{0}'.format(filename),
+            bucket,
+            'capital-nature/{0}'.format(filename)
+        )
+    else:
+        with open('{0}'.format(filename), mode = 'w') as f:
+            writer = csv.DictWriter(f, fieldnames = event_output[0].keys())
+            writer.writeheader()
+            [writer.writerow(event) for event in event_output]
+    return json.dumps(event_output, indent=2)
 
 # For local testing
-# event = {
-#   'url': 'https://anshome.org/events-calendar/',
-#   'source_name': 'ans'
-# }
-# is_local = True
-# print(handler(event, {}))
+if __name__ == "__main__":
+    event = {
+        'url': 'https://events.time.ly/8ib56fp',
+        'source_name': 'rcc'
+    }
+    is_local = True
+    print(handler(event, {}))
