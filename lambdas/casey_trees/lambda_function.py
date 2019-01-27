3bf29197794461ad469d2ba173e491e32bafc3e9
kirakrishnan <aravindakrishnan92@gmail.com>
2019-01-27 14:33:53 -0500

    updating function to extract events from casey trees website

--- lambdas/casey_trees/lambda_function.py
+++ lambdas/casey_trees/lambda_function.py
@@ -1,167 +1,59 @@
-from datetime import datetime
-import re
-import requests
-import csv
+import  requests
 from bs4 import BeautifulSoup
-import boto3
+import ast
+from datetime import datetime
+import pprint
+
 
 bucket = 'aimeeb-datasets-public'
 is_local = False
 
-def get_arlington_events():
-    '''
-    Gets animal- and environment-related events for Arlington County (https://today.arlingtonva.us/)
+def extract_data():
+    url = "https://caseytrees.org/events/2019-01/"
+    html_doc = requests.get(url).content
+    return html_doc
 
-    Parameters:
-        None
 
-    Returns:
-        event_items (list): a list of dictionaries, each of which represents an event.
-    '''
-    startDate = datetime.now().strftime("%Y-%m-%d")
-    from_param = 0
-    uri = f'https://today-service.arlingtonva.us/api/event/elasticevent?&StartDate={startDate}T05:00:00.000Z&EndDate=null&TopicCode=ANIMALS&TopicCode=ENVIRONMENT&ParkingAvailable=false&NearBus=false&NearRail=false&NearBikeShare=false&From={from_param}&Size=5&OrderBy=featured&EndTime=86400000'
-    r = requests.get(uri)
-    data = r.json()
-    count = data['count']
-    event_items = []
-    while from_param < count:
-        if from_param == 0:
-            items = data['items']
-            for item in items:
-                event_items.append(item)
-        else:
-            uri = f'https://today-service.arlingtonva.us/api/event/elasticevent?&StartDate={startDate}T05:00:00.000Z&EndDate=null&TopicCode=ANIMALS&TopicCode=ENVIRONMENT&ParkingAvailable=false&NearBus=false&NearRail=false&NearBikeShare=false&From={from_param}&Size=5&OrderBy=featured&EndTime=86400000'
-            r = requests.get(uri)
-            data = r.json()
-            items = data['items']
-            for item in items:
-                event_items.append(item) 
-        from_param += 5
-            
-    return event_items
+def extract_events():
+    page = extract_data()
+    soup = BeautifulSoup(page, "html.parser")
+    events_url = soup.find_all('td')
+    websites = []
+    for row in events_url:
+        for column in row.find_all('div'):
+            temp = column.text.strip()
+            if(temp.isdigit()):
+                pass
+            else:
+                websites.append(column.find('a')['href'])
 
-def html_textraction(html):
-    '''
-    Extracts text from html using bs4
+    events_content = soup.find_all('script',{'type':'application/ld+json'})
+    final_content = set()
+    for event in events_content:
+        for website in websites:
+            if(website in event.text.strip()):
+                final_content.add(event.text.strip())
 
-    Parameters:
-        html (str): a string containing html
+    final_content = ast.literal_eval(list(final_content)[0])
 
-    Returns:
-        text (str): the text extracted from the html
-    '''
-    if not html:
-        text = "See event website."
-    else:
-        soup = BeautifulSoup(html, 'html.parser')
-        text = soup.get_text().strip()
-    
-    return text
+    #extracts the required fields in the output schema
+    result_all_event = []
+    for con in final_content:
+        ind_event = {}
+        ind_event['url'] = con['url']
+        start = datetime.strptime(con['startDate'][:-6],"%Y-%m-%dT%H:%M:%S")
+        end = datetime.strptime(con['endDate'][:-6],"%Y-%m-%dT%H:%M:%S")
+        ind_event['startDate'] = start.strftime('%Y-%m-%d')
+        ind_event['endDate'] = end.strftime('%Y-%m-%d')
+        ind_event['startTime'] = start.strftime('%H:%M')
+        ind_event['endTime'] = end.strftime('%H:%M')
+        ind_event['address'] = ' '.join(str(x) for x in con['location']['address'].values())
+        ind_event['latitude'] = con['location']['geo']['latitude']
+        ind_event['venueName'] = con['name']
+        ind_event['longitude'] = con['location']['geo']['longitude']
+        result_all_event.append(ind_event)
 
-def parse_event_name(event_name):
-    '''
-    Clarifies the invasive plant removal event names and extracts text from html.
-    
-    Parameters:
-        event_name (str): the event name as a string
+    return result_all_event
 
-    Returns:
-        event_name (str): the parsed event name
-    '''
-    if any(x in event_name for x in ('RIP','RiP','Invasive Plant Removal')):
-        if "Invasive Plant Removal" in event_name:
-            parsed_event_name = re.sub('  +','',"".join(i for i in event_name if ord(i)<128)).replace("RiP",'').replace(" - ",'').replace("RIP",'')
-            parsed_event_name = re.sub("  +", " ", parsed_event_name).strip()
-        else:
-            parsed_event_name = re.sub('  +','',"".join(i for i in event_name if ord(i)<128))
-            parsed_event_name = parsed_event_name.replace("RiP",'').replace("RIP",'').replace(' - ','')
-            parsed_event_name = f'{parsed_event_name} Invasive Plant Removal'
-            parsed_event_name = re.sub("  +", " ", parsed_event_name).strip()
-        event_name = html_textraction(parsed_event_name)
-    else:
-        event_name = html_textraction(event_name)
-    
-    return event_name
 
-def schematize_events(event_items):
-    '''
-    Parses the events API output so that it conforms to our schema
-    
-    Parameters:
-        event_items (list): a list of dictionaries, each of which represents an event. 
-                            Output by get_arlington_events()
-
-    Returns:
-        events (list): a list of dictionaries, each of which represents an event in our schema
-    '''
-    events = []
-    for event_item in event_items:
-        event_item = event_item['vwEventWithLocation']
-        event_name = parse_event_name(event_item['eventName'])
-        if 'Task Force' in event_name or 'Forestry Commission' in event_name:
-            continue
-        event_description = html_textraction(event_item['eventDsc'])
-        start_date = event_item['eventStartDate']
-        end_date = event_item['eventEndDate']
-        start_time = event_item['eventStartTime']
-        end_time = event_item['eventEndTime']
-        event_website = event_item['eventUrlText']
-        if event_item['freeOfChargeInd']:
-            event_cost = 'Free' 
-        elif event_item['eventCostDsc']:
-            event_cost = event_item['eventCostDsc']
-        else:
-            event_cost =  "See event website."
-        event_venue = html_textraction(event_item['locationName'])
-        if event_venue == 'Earth Products Yard' or 'Library' in event_venue:
-            continue
-
-        event = {'Event Start Date':start_date,
-                 'Event End Date': end_date,
-                 'Event Start Time':start_time,
-                 'Event End Time':end_time,
-                 'Event Website':event_website,
-                 'Event Name':event_name,
-                 'Event Venue Name':event_venue,
-                 'Event Cost':event_cost,
-                 'Event Description':event_description}
-        events.append(event)
-        
-    return events
-
-def arlington_handler(event, context):
-    '''
-    AWS lambda function for Arlington County events.
-    '''
-    _ = event['url']
-    source_name = event['source_name']
-    event_items = get_arlington_events()
-    events = schematize_events(event_items)
-    filename = '{0}-results.csv'.format(source_name)
-    fieldnames = list(events[0].keys())
-    if not is_local:
-        with open('/tmp/{0}'.format(filename), mode = 'w') as f:
-            writer = csv.DictWriter(f, fieldnames = fieldnames)
-            writer.writeheader()
-            for arlington_event in events:
-                writer.writerow(arlington_event)
-        s3 = boto3.resource('s3')
-        s3.meta.client.upload_file('/tmp/{0}'.format(filename), 
-                                    bucket, 
-                                    'capital-nature/{0}'.format(filename)
-                                    )
-    else:
-        with open(filename, mode = 'w') as f:
-            writer = csv.DictWriter(f, fieldnames = fieldnames)
-            writer.writeheader()
-            for arlington_event in events:
-                writer.writerow(arlington_event)    
-
-#For local testing (it'll write the csv as arlington-results.csv into your working dir
-#event = {
-#'url': 'https://today.arlingtonva.us/',
-#'source_name': 'arlington'
-#}
-#is_local = True
-#arlington_handler(event, None)
+pprint.pprint(extract_events())
