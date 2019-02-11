a97fae937ece7fe2437e6559b3b1707a7ef631b4
csmcallister <mcallistercs@gmail.com>
2019-02-10 20:48:21 -0500

    CLN: remove older scripts

--- es_scripts/fona_eventbrite.py
+++
@@ -1,6 +0,0 @@
-from GenericEventbriteIngester import GenericEventbriteIngester
-
-fona_eventbrite_org_id = 13276552841
-
-FonaIngester = GenericEventbriteIngester(fona_eventbrite_org_id)
-FonaIngester.run()
