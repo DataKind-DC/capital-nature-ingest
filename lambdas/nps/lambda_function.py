d036dba31036e119ad60bb1ec583acf01fc388e9
csmcallister <mcallistercs@gmail.com>
2019-02-19 22:25:41 -0500

    FIX: update event organizer field name

--- lambdas/nps/lambda_function.py
+++ lambdas/nps/lambda_function.py
@@ -201,7 +201,7 @@ def schematize_nps_event(nps_event):
                                             "Event End Time":event_end_time,
                                             "All Day Event":event_all_day,
                                             "Event Venue Name":venue_name,
-                                            "Event Organizer Name(s) or ID(s)":event_organization,
+                                            "Event Organizers":event_organization,
                                             "Timezone":'America/New_York',
                                             "Event Cost":event_cost,
                                             "Event Currency Symbol":"$",
