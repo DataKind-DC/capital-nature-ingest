57881d4145495f5d3d975a268a18b1c401921cb5
sri <sripranathi.vr109@gmail.com>
2019-03-01 18:00:27 -0500

    Ensuring tab spacing

--- events/friends_of_kenilworth_gardens.py
+++ events/friends_of_kenilworth_gardens.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-import json
 import bs4
 import sys
 from datetime import datetime
@@ -48,51 +47,49 @@ def schematize_event_time(event_time):
 
 # Given a beautiful soup object, return a list of events on that page
 def main():
-      soup = soupify_event_page()
+    soup = soupify_event_page()
 
-      if not soup:
-          sys.exit(1)
+    if not soup:
+        sys.exit(1)
 
+    #Extract events details
+    events_details = soup.findAll('script',{'type':'application/ld+json'})
 
-      #Extract events details
-      events_details = soup.findAll('script',{'type':'application/ld+json'})
+    # Gather all the required fields into output schema
+    event_output = []
+    for event_details in eval(events_details[1].text.strip()):
+        # Get end date and times of the event and return only current or future events
+        eventEnd = datetime.strptime(event_details['endDate'][:-6], "%Y-%m-%dT%H:%M:%S")
 
-      # Gather all the required fields into output schema
-      event_output = []
-      for event_details in eval(events_details[1].text.strip()):
-          # Get end date and times of the event and return only current or future events
-          eventEnd = datetime.strptime(event_details['endDate'][:-6], "%Y-%m-%dT%H:%M:%S")
+    # Return only unexpired events
+        if eventEnd > datetime.now():
 
-      # Return only unexpired events
-          if eventEnd > datetime.now():
-
-              event_data = {
-                'Event Name': event_details['name'],
-                'Event Description': event_details['description'],
-                'Event Start Date': schematize_event_date(event_details['startDate']),
-                'Event Start Time': schematize_event_time(event_details['startDate']),
-                'Event End Date': schematize_event_date(event_details['endDate']),
-                'Event End Time': schematize_event_time(event_details['endDate']),
-                'Event Time Zone': "America/New_York",
-                'Event Venue Name': event_details['location']["name"],
-                'Event Organizer': event_details['organizer']["name"],
-                'Event Price': event_details['offers']['lowPrice'],
-                'Event Currency Symbol': event_details['offers']['priceCurrency'],
-                'Event Currency Position': "prefix",
-                'Event Website': event_details['url'],
-                'Event Featured Image': event_details['image'],
-                'All Day Event': False,
-                # commenting addresss, latitude and longitude fields for now as The WordPress Event plugin doesn't
-                # expect these fields, but we might eventually use their Map plugin, which would need those geo fields
-                #'Event Latitude': event_details['location']['geo']['latitude'],
-                #'Event Longitude': event_details['location']['geo']['longitude'],
-                #'Event Address': event_details['location']['address']['streetAddress']+', '+
-                #                 event_details['location']['address']['addressLocality']+" "+
-                #                 event_details['location']['address']['addressRegion']
-              }
-              event_output.append(event_data)
-      print(event_output)
-      return event_output
+            event_data = {
+              'Event Name': event_details['name'],
+              'Event Description': event_details['description'],
+              'Event Start Date': schematize_event_date(event_details['startDate']),
+              'Event Start Time': schematize_event_time(event_details['startDate']),
+              'Event End Date': schematize_event_date(event_details['endDate']),
+              'Event End Time': schematize_event_time(event_details['endDate']),
+              'Event Time Zone': "America/New_York",
+              'Event Venue Name': event_details['location']["name"],
+              'Event Organizer': event_details['organizer']["name"],
+              'Event Price': event_details['offers']['lowPrice'],
+              'Event Currency Symbol': event_details['offers']['priceCurrency'],
+              'Event Currency Position': "prefix",
+              'Event Website': event_details['url'],
+              'Event Featured Image': event_details['image'],
+              'All Day Event': False,
+              # commenting addresss, latitude and longitude fields for now as The WordPress Event plugin doesn't
+              # expect these fields, but we might eventually use their Map plugin, which would need those geo fields
+              #'Event Latitude': event_details['location']['geo']['latitude'],
+              #'Event Longitude': event_details['location']['geo']['longitude'],
+              #'Event Address': event_details['location']['address']['streetAddress']+', '+
+              #                 event_details['location']['address']['addressLocality']+" "+
+              #                 event_details['location']['address']['addressRegion']
+            }
+            event_output.append(event_data)
+        return event_output
 
 
 
