24af12dd3bfa0b4d82ae28b34fa5af196026567f
csmcallister <mcallistercs@gmail.com>
2019-09-20 10:54:42 -0400

    DOC: name test cases correctly :pencil2:

--- tests/ans_test.py
+++ tests/ans_test.py
@@ -5,9 +5,9 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from events.ans import main
 from .utils import schema_test_required, schema_test_all, schema_test_types
 
-class DUGTestCase(unittest.TestCase):
+class ANSTestCase(unittest.TestCase):
     '''
-    Test cases for DUG events.
+    Test cases for ANS events.
     '''
 
     def setUp(self):
