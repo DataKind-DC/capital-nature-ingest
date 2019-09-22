5010594d5606bf91dd07e63b79f247876c246629
Jennifer Melot <jtmelot@gmail.com>
2019-09-22 17:04:38 -0400

    ENH: adding us botanic garden scraper (#113)

---
+++ events/us_botanic_garden.py
@@ -0,0 +1,177 @@
+import requests
+import logging
+import json
+import datetime
+import copy
+import re
+from bs4 import BeautifulSoup
+
+
+def get_event_ids(num_months):
+    '''
+    The US Botanic Garden calendar shown at
+    https://www.usbg.gov/programs
+    is populated from a JSON returned by a request to DoubleKnot's server on page load.
+
+    This function makes this request to retrieve an ID for each event, which is used to generating the event detail
+    URL in `get_event_info`. I considered just retrieving events for each month until I found a month without events,
+    but recurring events mean that might never happen.
+    :param num_months: number of months to retrieve data for - should be an int > 0
+    :return: set of event ids
+    '''
+    req_params = (
+        ("calendarid", "dkdpm"),
+    )
+
+    # most of these parameters are copied directly from the request that ran when the calendar loaded in
+    # my browser. Even changing "visibleEnd" to greater than a month out seems to only retrieve a month's worth
+    # of data, so just going to run the request with the start date offset from 0 to num_months - 1
+    base_req_data = {
+        "action": "Init",
+        "header": {
+            "control": "dpm",
+            "id": "DKdpm",
+            "v": "218-lite",
+            "visibleStart": None,
+            "visibleEnd": "9999-12-31T23:59:59",
+            "startDate": None,
+            "reqStartDate": "9999-12-31T23:59:59",
+            "reqEndDate": "9999-12-31T23:59:59",
+            "orgKey": 4153,
+            "orgList": "4153",
+            "categoryList": "",
+            "reserveOptionsList": "",
+            "headerBackColor": "#F3F3F9",
+            "backColor": "#ffffff",
+            "nonBusinessBackColor": "#ffffff",
+            "timeFormat": "Auto",
+            "weekStarts": 0
+        }
+    }
+
+
+    # seen_event_ids needs to be a set since sometimes you get overlapping events from adjacent months used to
+    # pad the start and end of the calendar view
+    seen_event_ids = set()
+    today = datetime.datetime.now()
+    base_month = today.month
+    base_year = today.year
+    for month_offset in range(num_months):
+        req_data = copy.deepcopy(base_req_data)
+        # request everything starting from the first of the current month so users can see entire month's history
+        curr_year = base_year
+        curr_month = month_offset+base_month
+        if curr_month > 12:
+            curr_year += int(curr_month / 12)
+            curr_month = curr_month % 12
+        first_of_month = f"{curr_year}-{curr_month}-01T00:00:00"
+        req_data["header"]["visibleStart"] = first_of_month
+        req_data["header"]["startDate"] = first_of_month
+        # doubleknot require the word "JSON" to be prepended to the stringified JSON data
+        response = requests.post('https://usbg.doubleknot.com/app/calendar/eventdata', params=req_params,
+                                 data="JSON" + json.dumps(req_data))
+        for evt in response.json()["Events"]:
+            seen_event_ids.add(evt["id"])
+
+    # return in order of id
+    return sorted(list(seen_event_ids))
+
+
+def get_event_info(evt_id):
+    '''
+    Given an event id, go to the event detail page and extract all its information
+    :param evt_id: id of event, used in event detail page url
+    :return: dict of event information
+    '''
+    url = f"https://usbg.doubleknot.com/registration/calendardetail.aspx?activitykey={evt_id}&orgkey=4153"
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+
+    # get cost. Since we can only provide one, use the non-member cost
+    cost_span = soup.find("span", {"id": "lblCost"})
+    cost = "0"
+    if cost_span is not None:
+        cost = re.search(r"([0-9\.]+) per non-?member", cost_span.get_text().lower()).group(1)
+
+    # get schedule info
+    startdt = soup.find("time", {"id": "timeStartTime"})["content"]
+    start_date, start_time_with_zone = startdt.split("T")
+    start_time = start_time_with_zone.split("-")[0]
+    enddt = soup.find("span", {"id": "lblEventDate"})["content"]
+    end_date, end_time_with_zone = enddt.split("T")
+    end_time = end_time_with_zone.split("-")[0]
+
+    # get the rest of the info, which unfortunately is embedded in a sequence of p elements without identifiers
+    # except the content and position
+    img_host = "https://5a6a246dfe17a1aac1cd-b99970780ce78ebdd694d83e551ef810.ssl.cf1.rackcdn.com/"
+    description, featured_image, venue, evt_name, evt_category = "", "", "", "", ""
+    # there is only one element in divNotes with class dk_cd_itemdetail
+    right_panel = soup.find("div", {"id": "divNotes"}).find("div", {"class": "dk_cd_itemdetail"})
+
+    for idx, p in enumerate(right_panel.find_all("p")):
+        text = p.get_text().replace("\n", " ").strip()
+        images = p.find_all("img")
+        if len(images) > 0:
+            featured_image = img_host+images[0]["src"]
+        elif text.startswith("LOCATION:"):
+            venue = text.split("LOCATION:")[1].strip()
+        elif idx == 0:
+            # I thought I could get the evt name and category from the th with class dk-title-bar, but it's not
+            # formatted consistently. The fist p element is, like this - CATEGORY: TITLE
+            title_re = re.search(r"^([^:]+):\s*(.*)", text)
+            evt_category = title_re.group(1)
+            evt_name = title_re.group(2)
+        elif idx == 1:
+            # it's the subtitle - skip it
+            continue
+        elif not re.search("^[A-Z- ]+:", text):
+            # Add all text that doesn't start with some kind of label and isn't the subtitle to the description
+            description += text+"\n"
+
+    evt_info = {
+        "Event Name": evt_name,
+        "Event Description": description,
+        "Event Start Date": start_date,
+        "Event Start Time": start_time,
+        "Event End Date": end_date,
+        "Event End Time": end_time,
+        "All Day Event": False,
+        "Timezone": "America/New_York",
+        "Event Venue Name": "United States Botanic Garden: "+venue,
+        "Event Organizers": "United States Botanic Garden",
+        "Event Cost": cost,
+        "Event Currency Symbol": "$",
+        "Event Category": evt_category,
+        "Event Website": url,
+        "Event Featured Image": featured_image
+    }
+
+    # make sure the values are cleaned up
+    for key in evt_info:
+        if type(evt_info[key]) is str:
+            evt_info[key] = evt_info[key].replace("\xa0", " ").strip()
+
+    return evt_info
+
+
+def main():
+    # retrieve event IDs
+    event_ids = []
+    try:
+        event_ids = get_event_ids(6)
+    except Exception as e:
+        logging.exception("Failed to retrieve US Botanic Garden event IDs.")
+
+    # retrieve event information
+    event_info = []
+    for evt_id in event_ids:
+        try:
+            event_info.append(get_event_info(evt_id))
+        except Exception as e:
+            logging.exception(f"Failed to retrieve event information for US Botanic Garden event {evt_id}.")
+    return event_info
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    events = main()
