44d3462a5d6d7b6657205d827fa0e5d6f2ecf934
Scott McAllister <mcallistercs@gmail.com>
2019-09-24 20:51:07 -0400

    CLN: rename test case :art:

--- tests/us_botanic_garden_test.py
+++ tests/us_botanic_garden_test.py
@@ -5,9 +5,9 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from events.us_botanic_garden import main
 from .utils import schema_test_required, schema_test_all, schema_test_types
 
-class DUGTestCase(unittest.TestCase):
+class USBotanicGardenTestCase(unittest.TestCase):
     '''
-    Test cases for DUG events.
+    Test cases for US Botanic Garden events.
     '''
 
     def setUp(self):
