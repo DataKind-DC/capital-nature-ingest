1710a5e95941e56af925d4b4a6e25b4e31eedc24
Scott McAllister <mcallistercs@gmail.com>
2019-11-15 16:34:04 -0500

    FIX: rename test case :pencil2:

--- tests/sierra_club_md_test.py
+++ tests/sierra_club_md_test.py
@@ -5,9 +5,9 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from events.sierra_club_md import main
 from .utils import schema_test_required, schema_test_all, schema_test_types
 
-class ANSTestCase(unittest.TestCase):
+class SierraClubMDTestCase(unittest.TestCase):
     '''
-    Test cases for ANS events.
+    Test cases for Sierra Club MD events.
     '''
 
     def setUp(self):
