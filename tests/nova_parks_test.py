baa1b71d9bf32bb9e30d807c8b776b0b9a5c3ad0
Scott McAllister <mcallistercs@gmail.com>
2019-11-15 16:34:34 -0500

    FIX: rename test case :pencil2:

--- tests/nova_parks_test.py
+++ tests/nova_parks_test.py
@@ -5,9 +5,9 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from events.nova_parks import main
 from .utils import schema_test_required, schema_test_all, schema_test_types
 
-class ANSTestCase(unittest.TestCase):
+class NOVATestCase(unittest.TestCase):
     '''
-    Test cases for ANS events.
+    Test cases for NOVA events.
     '''
 
     def setUp(self):
