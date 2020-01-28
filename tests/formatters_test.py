15f59462225953b3daa4b8e624f2fa507ab4cd06
csmcallister <mcallistercs@gmail.com>
2020-01-27 19:59:44 -0500

    TST: test formatters :white_check_mark:

--- tests/formatters_test.py
+++ tests/formatters_test.py
@@ -3,10 +3,10 @@ from os import path
 import sys
 import unittest
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-import get_events
+from utils.formatters import unicoder, date_filter
 
-class GetEventsTestCase(unittest.TestCase):
-
+class FormattersTestCase(unittest.TestCase):
+    
     def setUp(self):
         self.maxDiff = None
 
@@ -14,32 +14,32 @@ class GetEventsTestCase(unittest.TestCase):
         pass
 
     def test_unicoder(self):
-        result = get_events.unicoder('test')
+        result = unicoder('test')
         expected = 'test'
         self.assertEqual(result, expected)
 
     def test_unicoder_apostrophe(self):
-        result = get_events.unicoder('Weâ€™ll')
+        result = unicoder('Weâ€™ll')
         expected = "We’ll"
         self.assertEqual(result, expected)
 
     def test_unicoder_dash(self):
-        result = get_events.unicoder('Earth Day â€“ Monday Mini Camp')
+        result = unicoder('Earth Day â€“ Monday Mini Camp')
         expected = "Earth Day – Monday Mini Camp"
         self.assertEqual(result, expected)
 
     def test_unicoder_empty(self):
-        result = get_events.unicoder('Â')
+        result = unicoder('Â')
         expected = ""
         self.assertEqual(result, expected)
 
     def test_unicoder_quotes(self):
-        result = get_events.unicoder('â€œ')
+        result = unicoder('â€œ')
         expected = '“'
         self.assertEqual(result, expected)
 
     def test_unicoder_accents(self):
-        result = get_events.unicoder('áéíóúüñ¿¡')
+        result = unicoder('áéíóúüñ¿¡')
         expected = 'áéíóúüñ¿¡'
         self.assertEqual(result, expected)
 
@@ -50,15 +50,15 @@ class GetEventsTestCase(unittest.TestCase):
         bad_date = bad_date.strftime('%Y-%m-%d')
         data = [{'Event Start Date': good_date},
                 {'Event Start Date': bad_date}]
-        result = get_events.date_filter(data)
+        result = date_filter(data)
         expected = [{'Event Start Date': good_date}]
         self.assertEqual(result, expected)
 
     def test_date_filter_bad_values(self):
         data = [{'Event Start Date': 'bad date'}]
-        result = get_events.date_filter(data)
+        result = date_filter(data)
         expected = []
         self.assertEqual(result, expected)
-        
+
 if __name__ == '__main__':
     unittest.main()
