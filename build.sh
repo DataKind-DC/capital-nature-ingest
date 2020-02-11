e82db400729e7375edf9bc9d592200826c6a934e
csmcallister <mcallistercs@gmail.com>
2020-02-10 20:43:26 -0500

    exclude pycache in rysync

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir lambda-releases && mkdir temp && mkdir temp/utils
-rsync -av --progress events/ temp/
-rsync -av --progress utils/ temp/utils/
+rsync -av --exclude __pycache__ --progress events/ temp/ 
+rsync -av --exclude __pycache__ --progress utils/ temp/utils/ 
 cp get_events.py temp/get_events.py
 cp log.py temp/log.py
 cp requirements.txt temp/requirements.txt
